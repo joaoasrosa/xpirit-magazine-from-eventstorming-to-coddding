8a70f3bc721f3f4dbf7e79d863f10bb6ce3fe7b2
João Rosa <joaoasrosa@gmail.com>
2019-08-02 11:08:26 +0200

    Add first test

---
+++ test/Domain.Unit.Tests/TicketBoothShould.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Domain.Unit.Tests
+{
+    public class TicketBoothShould
+    {
+        [Fact]
+        public void Return_SeatsNotAvailable_when_MovieScreening_has_all_its_seats_already_reserved()
+        {
+            
+        }
+    }
+}
