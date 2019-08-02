befbc1dbfe4dbb575aff4a2bfa834245eb85d710
João Rosa <joaoasrosa@gmail.com>
2019-08-02 12:06:29 +0200

    Internal initialisation

--- src/Domain/MovieScreening.cs
+++ src/Domain/MovieScreening.cs
@@ -2,9 +2,13 @@ namespace Domain
 {
     public class MovieScreening
     {
+        private readonly uint _rows;
+        private readonly uint _seatsPerRow;
+
         public MovieScreening(uint rows, uint seatsPerRow)
         {
-            throw new System.NotImplementedException();
+            _rows = rows;
+            _seatsPerRow = seatsPerRow;
         }
     }
 }
