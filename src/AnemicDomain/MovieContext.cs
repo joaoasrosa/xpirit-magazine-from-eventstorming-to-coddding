cc5e1616d7bc421cf36f6b4e74ae0409bb83c4ac
João Rosa <joaoasrosa@gmail.com>
2019-09-07 17:11:15 +0200

    Add Movie Context

---
+++ src/AnemicDomain/MovieContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace AnemicDomain
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
