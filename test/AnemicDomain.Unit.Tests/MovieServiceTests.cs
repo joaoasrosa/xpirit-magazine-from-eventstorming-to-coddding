ad33f010494a414dd1561e76ae8879869718c381
João Rosa <joaoasrosa@gmail.com>
2019-09-07 17:17:52 +0200

    Add test

---
+++ test/AnemicDomain.Unit.Tests/MovieServiceTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace AnemicDomain.Unit.Tests
+{
+    public class MovieServiceTests
+    {
+        [Fact]
+        public void IfMovieScreeningIsEmpty_ReserveSeats()
+        {
+            
+        }
+    }
+}
