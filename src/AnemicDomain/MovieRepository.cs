6babbd1c5b46186d92d27bdb834ddad4197e7bf4
João Rosa <joaoasrosa@gmail.com>
2019-09-07 17:15:10 +0200

    Implement repository

--- src/AnemicDomain/MovieRepository.cs
+++ src/AnemicDomain/MovieRepository.cs
@@ -1,15 +1,29 @@
+using System.Linq;
+
 namespace AnemicDomain
 {
     public class MovieRepository
     {
+        private readonly MovieContext _movieContext;
+
+        public MovieRepository(MovieContext movieContext)
+        {
+            _movieContext = movieContext;
+        }
+
         public Movie FindByName(string mvNaam)
         {
-            throw new System.NotImplementedException();
+            return _movieContext.Movies.FirstOrDefault(x => x.Name.Equals(mvNaam));
         }
 
         public void Save(Movie movie)
         {
-            throw new System.NotImplementedException();
+            if (_movieContext.Movies.Any(x => x.Id.Equals(movie.Id)))
+                _movieContext.Movies.Update(movie);
+            else
+                _movieContext.Movies.Add(movie);
+
+            _movieContext.SaveChanges();
         }
     }
 }
